9e73f86639494f697407f2686b35735aea168334
Code Sandbox <hello@codesandbox.io>
2021-06-23 21:50:38 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.6d61e26314c14df2848623f90f2f7d7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38316cf2345a3daa95194b2f0b0fba73",
+    "url": "https://constanzadl.github.io/csb-ds76x/index.html"
+  },
+  {
+    "revision": "9ee172af5b54d669a559",
+    "url": "https://constanzadl.github.io/csb-ds76x/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "251c10a770965d97fb1c",
+    "url": "https://constanzadl.github.io/csb-ds76x/static/js/2.ffd3e9df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://constanzadl.github.io/csb-ds76x/static/js/2.ffd3e9df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ee172af5b54d669a559",
+    "url": "https://constanzadl.github.io/csb-ds76x/static/js/main.c5b5497d.chunk.js"
+  },
+  {
+    "revision": "ea5c9ced0e70ef4d9cd9",
+    "url": "https://constanzadl.github.io/csb-ds76x/static/js/runtime-main.2c4ce69e.js"
+  }
+]);
